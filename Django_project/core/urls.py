1d1532f7247dce5ade593526e854ea80e776725c
tumana777 <tumana991@gmail.com>
2025-03-04 22:14:11 +0400

    Create forms

--- Django_project/core/urls.py
+++ Django_project/core/urls.py
@@ -1,9 +1,12 @@
 
 from django.urls import path
-from core.views import index, about
+from core.views import index, about, car_details, add_car
 
 app_name = 'core'
+
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
+    path('car_details/<int:car_pk>/', car_details, name='car_details'),
+    path('add_car/', add_car, name='add_car'),
 ]
--- Django_project/core/views.py
+++ Django_project/core/views.py
@@ -1,13 +1,35 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Car
+from .forms import CarForm
 
 def index(request):
 
-    cars = Car.objects.all()
+    cars = Car.objects.all().select_related('category')
 
     return render(request, 'index.html', {'cars': cars})
 
 def about(request):
 
     return render(request, 'about.html')
+
+def car_details(request, car_pk):
+
+    car = get_object_or_404(Car, pk=car_pk)
+
+    return render(request, 'car_detail.html', {'car': car})
+
+def add_car(request):
+    if request.method == 'POST':
+        print(request.method)
+        form = CarForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('core:index')
+    else:
+        form = CarForm()
+
+    print(request.method)
+
+    return render(request, 'add_car.html', {'form': form})
